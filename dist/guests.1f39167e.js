73bac7f344d9fc61b89a10e34fd879e5786a266f
Bari100 <baree1995@gmail.com>
2020-07-22 19:01:45 +0300

    added possibility to add guests and babies in the guests.pug

--- dist/guests.1f39167e.js
+++ dist/guests.1f39167e.js
@@ -122,7 +122,7 @@ $(document).ready(function () {
   $('.guests__minus').click(function () {
     var $input = $(this).parent().find('input');
     var count = parseInt($input.val()) - 1;
-    count = count < 1 ? 1 : count;
+    count = count < 1 ? 0 : count;
     $input.val(count);
     $input.change();
     return false;
@@ -132,6 +132,79 @@ $(document).ready(function () {
     $input.val(parseInt($input.val()) + 1);
     $input.change();
     return false;
+  });
+}); // Dropdown Menu
+
+var dropdown = document.querySelectorAll('.guests__dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown, 0);
+dropdownArray.forEach(function (el) {
+  var button = el.querySelector('a[data-toggle="dropdown"]'),
+      menu = el.querySelector('.guests__dropdown-menu'),
+      arrow = button.querySelector('i.icon-arrow');
+
+  button.onclick = function (event) {
+    if (!menu.hasClass('show')) {
+      menu.classList.add('show');
+      menu.classList.remove('hide');
+      arrow.classList.add('open');
+      arrow.classList.remove('close');
+      event.preventDefault();
+    } else {
+      menu.classList.remove('show');
+      menu.classList.add('hide');
+      arrow.classList.remove('open');
+      arrow.classList.add('close');
+      event.preventDefault();
+    }
+  };
+});
+
+Element.prototype.hasClass = function (className) {
+  return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
+
+$(function () {
+  var i = -1;
+  var tt = ["1 гость", "2 гостя", "3 гостя", "4 гостя", "5 гостей", "6 гостей", "7 гостей"];
+
+  function getMessage() {
+    return tt[i];
+  }
+
+  var guestsText = document.getElementById('guests__text');
+  $(".guests__click").click(function () {
+    ++i;
+    $(guestsText).text(getMessage()); // if (i>=tt.length) {
+    // 	i=-1;
+    // }
+  });
+  $(".guests__click-minus").click(function () {
+    i--;
+    $(guestsText).text(getMessage()); // if (i<=tt.length) {
+    // 	i=-1;
+    // }
+  });
+});
+$(function () {
+  var iBaby = -1;
+  var objectBaby = [", 1 младенец", ", 2 младенца", ", 3 младенца", ", 4 младенца", ", 5 младенцев", ", 6 младенцев", ", 7 младенцев"];
+
+  function getMessageBaby() {
+    return objectBaby[iBaby];
+  }
+
+  var guestsTextBaby = document.getElementsByClassName('guests__text-baby');
+  $(".guests__click-baby").click(function () {
+    ++iBaby;
+    $(guestsTextBaby).text(getMessageBaby()); // if (i>=tt.length) {
+    // 	i=-1;
+    // }
+  });
+  $(".guests__click-baby-minus").click(function () {
+    iBaby--;
+    $(guestsTextBaby).text(getMessageBaby()); // if (i<=tt.length) {
+    // 	i=-1;
+    // }
   });
 });
 },{}],"C:/Users/user/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -162,7 +235,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60647" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60688" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/blocks/_commons/guests/guests.js
+++ src/blocks/_commons/guests/guests.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.guests__minus').click(function () {
         var $input = $(this).parent().find('input');
         var count = parseInt($input.val()) - 1;
-        count = count < 1 ? 1 : count;
+        count = count < 1 ? 0 : count;
         $input.val(count);
         $input.change();
         return false;
@@ -13,4 +13,81 @@ $(document).ready(function() {
         $input.change();
         return false;
     });
+});
+
+
+// Dropdown Menu
+var dropdown = document.querySelectorAll('.guests__dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
+dropdownArray.forEach(function(el){
+	var button = el.querySelector('a[data-toggle="dropdown"]'),
+			menu = el.querySelector('.guests__dropdown-menu'),
+			arrow = button.querySelector('i.icon-arrow');
+
+	button.onclick = function(event) {
+		if(!menu.hasClass('show')) {
+			menu.classList.add('show');
+			menu.classList.remove('hide');
+			arrow.classList.add('open');
+			arrow.classList.remove('close');
+			event.preventDefault();
+		}
+		else {
+			menu.classList.remove('show');
+			menu.classList.add('hide');
+			arrow.classList.remove('open');
+			arrow.classList.add('close');
+			event.preventDefault();
+		}
+	};
+});
+
+Element.prototype.hasClass = function(className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
+
+$(function(){
+	var i = -1;
+	var tt = ["1 гость", "2 гостя", "3 гостя", "4 гостя", "5 гостей", "6 гостей", "7 гостей"];
+	function getMessage() {
+		return tt[i];
+	}
+	var guestsText = document.getElementById('guests__text'); 
+	$(".guests__click").click(function () {
+		++i;
+		$(guestsText).text(getMessage());
+		// if (i>=tt.length) {
+		// 	i=-1;
+		// }
+	});
+	$(".guests__click-minus").click(function () {
+		i--;
+		$(guestsText).text(getMessage());
+		// if (i<=tt.length) {
+		// 	i=-1;
+		// }
+	});
+});
+
+$(function(){
+	var iBaby = -1;
+	var objectBaby = [", 1 младенец", ", 2 младенца", ", 3 младенца", ", 4 младенца", ", 5 младенцев", ", 6 младенцев", ", 7 младенцев"];
+	function getMessageBaby() {
+		return objectBaby[iBaby];
+	}
+	var guestsTextBaby = document.getElementsByClassName('guests__text-baby'); 
+	$(".guests__click-baby").click(function () {
+		++iBaby;
+		$(guestsTextBaby).text(getMessageBaby());
+		// if (i>=tt.length) {
+		// 	i=-1;
+		// }
+	});
+	$(".guests__click-baby-minus").click(function () {
+		iBaby--;
+		$(guestsTextBaby).text(getMessageBaby());
+		// if (i<=tt.length) {
+		// 	i=-1;
+		// }
+	});
 });
