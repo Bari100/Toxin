6909a7ce5800d5fabdac4e0ba00872295a0b06f2
Bari100 <baree1995@gmail.com>
2020-08-31 13:58:05 +0300

    finished ui-kit

--- src/blocks/search-room/search-room__filter/search-room__filter-price/search-room__filter-price.js
+++ src/blocks/search-room/search-room__filter/search-room__filter-price/search-room__filter-price.js
@@ -2,8 +2,8 @@ $( function() {
   $( "#search-room__filter-price-slider-range" ).slider({
     range: true,
     min: 0,
-    max: 18000,
-    values: [ 75, 300 ],
+    max: 15000,
+    values: [ 5000, 10000 ],
     slide: function( event, ui ) {
       $( "#amount" ).val( ui.values[ 0 ] + "₽" + " - " + ui.values[ 1 ] + "₽" );
     }
